abf4cafa97554a0e4a6bfbce90164a41b2ba14d2
abhinov <abhinov.in@gmail.com>
2021-07-19 12:33:09 +0530

    added springdatajpa config, init frontend for angular project

---
+++ backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/springbootecommerce/config/MySpringDataRestConfig.java
@@ -0,0 +1,29 @@
+package com.springbootecommerce.config;
+
+import com.springbootecommerce.entity.Product;
+import com.springbootecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MySpringDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        // Disable http methods PUT, POST, DELETE for product repository in v1.0
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        // Disable http methods PUT, POST, DELETE for product Category repository in v1.0
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+    }
+}
