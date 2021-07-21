6742d501ca4e0ae8907e54c7eae8d674c8688bc2
abhinov <abhinov.in@gmail.com>
2021-07-21 10:23:08 +0530

    Expose entity ids in Spring Data Rest

--- backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/springbootecommerce/config/MySpringDataRestConfig.java
+++ backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/springbootecommerce/config/MySpringDataRestConfig.java
@@ -2,14 +2,29 @@ package com.springbootecommerce.config;
 
 import com.springbootecommerce.entity.Product;
 import com.springbootecommerce.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MySpringDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public MySpringDataRestConfig(EntityManager theEntityManager) {
+        this.entityManager = theEntityManager;
+    }
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
@@ -25,5 +40,31 @@ public class MySpringDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        // call an internal helper method
+        exposeIds(config);
     }
+
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        // Expose entity ids in Spring Data Rest
+
+        // -get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        // -create array of the entity types
+        List<Class> entityClass = new ArrayList<>();
+
+        // -get the entity types for the entities
+        for(EntityType entityType: entities) {
+            entityClass.add(entityType.getJavaType());
+        }
+
+        // -expose the entity ids for the array of entity/domain types
+        Class[] domainTypes = entityClass.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
+    }
+
+
+
 }
