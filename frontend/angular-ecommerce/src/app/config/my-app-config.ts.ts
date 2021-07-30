06582b84cf2d15d7e56725c74600d27d23247904
abhinov <abhinov.in@gmail.com>
2021-07-30 18:20:00 +0530

    user registration bugfixed

--- frontend/angular-ecommerce/src/app/config/my-app-config.ts.ts
+++ frontend/angular-ecommerce/src/app/config/my-app-config.ts.ts
@@ -1,8 +1,8 @@
 export default {
-
+// https://dev-11871015-admin.okta.com/
   oidc: {
-    clientId: '0oayqbm9D3ASmOisW695',
-    issuer: 'https://gmailabhinov.okta.com/oauth2/default',
+    clientId: '0oa1dem87xGaWacHw5d7',
+    issuer: 'https://dev-11871015.okta.com/oauth2/default',
     redirectUri: 'http://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email']
   }
